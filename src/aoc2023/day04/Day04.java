7a4f31247907df859abba90780ac3886f9a8501e
Adrienne Bisaillon <adrienne.bisaillon@gmail.com>
2023-12-04 21:04:47 -0500

    Day 4 part 2

--- src/aoc2023/day04/Day04.java
+++ src/aoc2023/day04/Day04.java
@@ -3,7 +3,11 @@ package aoc2023.day04;
 import aoc2023.Day;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,6 +39,32 @@ public class Day04 extends Day {
 
     @Override
     public Object partTwo(Stream<String> lines) {
-        return null;
+        final Pattern cardPattern = Pattern.compile("Card\\s*(\\d+):(.*)\\|(.*)");
+        Map<Integer, Integer> cards = new HashMap<>();
+        lines.forEach(line -> {
+            final Matcher cardMatcher = cardPattern.matcher(line);
+            if (!cardMatcher.find()) {
+                return;
+            }
+            int cardNumber = Integer.parseInt(cardMatcher.group(1));
+            Set<String> winningNumbers = Arrays
+                    .stream(cardMatcher.group(2).split(" "))
+                    .filter(x -> !x.isEmpty())
+                    .collect(Collectors.toSet());
+            Set<String> gameNumbers = Arrays
+                    .stream(cardMatcher.group(3).split(" "))
+                    .filter(x -> !x.isEmpty())
+                    .collect(Collectors.toSet());
+            gameNumbers.retainAll(winningNumbers);
+            if (cards.containsKey(cardNumber)) {
+                cards.put(cardNumber, cards.get(cardNumber) + 1);
+            } else {
+                cards.put(cardNumber, 1);
+            }
+            for (int i = 1; i <= gameNumbers.size(); i++) {
+                cards.put(cardNumber + i, cards.getOrDefault(cardNumber + i, 0) + cards.get(cardNumber));
+            }
+        });
+        return cards.values().stream().reduce(0, Integer::sum);
     }
 }
