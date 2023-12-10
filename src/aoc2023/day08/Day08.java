4bff320180628ef95c5021c03813da59e0d102f3
Adrienne Bisaillon <adrienne.bisaillon@gmail.com>
2023-12-10 15:28:33 -0500

    Day 8 part 2

--- src/aoc2023/day08/Day08.java
+++ src/aoc2023/day08/Day08.java
@@ -15,56 +15,54 @@ public class Day08 extends Day {
 
     @Override
     public Object partOne(Stream<String> lines) {
-        return null;
+        List<String> allLines = lines.toList();
+        List<String> directions = Arrays.stream(allLines.get(0).split("")).toList();
+
+        Map<String, Node> network = new HashMap<>();
+        Node start = null;
+
+        for (String line : allLines.stream().skip(2).toList()) {
+
+            // make 3 nodes and add to map
+            final String regex = "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)";
+            final Pattern pattern = Pattern.compile(regex);
+            final Matcher matcher = pattern.matcher(line);
+
+            if (matcher.find()) {
+                Node root = new Node(matcher.group(1));
+                Node left = new Node(matcher.group(2));
+                Node right = new Node(matcher.group(3));
+
+                root.leftPointer = left;
+                root.rightPointer = right;
+                if (start == null) {
+                    start = root;
+                }
+                network.put(root.value, root);
+            }
+        }
+
+        // traverse based on instructions
+        int steps = 0;
+        assert start != null;
+        String currentNodeValue = start.value;
+        final String TARGET_VALUE = "ZZZ";
+
+
+        int i = 0;
+        while (!currentNodeValue.equals(TARGET_VALUE)) {
+            if (directions.get(i).equals("L")) {
+                currentNodeValue = network.get(currentNodeValue).leftPointer.value;
+            }
+            if (directions.get(i).equals("R")) {
+                currentNodeValue = network.get(currentNodeValue).rightPointer.value;
+            }
+            i = (i + 1) % directions.size();
+            steps++;
+        }
+
+        return steps;
     }
-//        List<String> allLines = lines.toList();
-//        List<String> directions = Arrays.stream(allLines.get(0).split("")).toList();
-//
-//        Map<String, Node> network = new HashMap<>();
-//        Node start = null;
-//
-//        for (String line : allLines.stream().skip(2).toList()) {
-//
-//            // make 3 nodes and add to map
-//            final String regex = "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)";
-//            final Pattern pattern = Pattern.compile(regex);
-//            final Matcher matcher = pattern.matcher(line);
-//
-//            if (matcher.find()) {
-//                Node root = new Node(matcher.group(1));
-//                Node left = new Node(matcher.group(2));
-//                Node right = new Node(matcher.group(3));
-//
-//                root.leftPointer = left;
-//                root.rightPointer = right;
-//                if (start == null) {
-//                    start = root;
-//                }
-//                network.put(root.value, root);
-//            }
-//        }
-//
-//        // traverse based on instructions
-//        int steps = 0;
-//        assert start != null;
-//        String currentNodeValue = start.value;
-//        final String TARGET_VALUE = "ZZZ";
-//
-//
-//        int i = 0;
-//        while (!currentNodeValue.equals(TARGET_VALUE)) {
-//            if (directions.get(i).equals("L")) {
-//                currentNodeValue = network.get(currentNodeValue).leftPointer.value;
-//            }
-//            if (directions.get(i).equals("R")) {
-//                currentNodeValue = network.get(currentNodeValue).rightPointer.value;
-//            }
-//            i = (i + 1) % directions.size();
-//            steps++;
-//        }
-//
-//        return steps;
-//    }
 
     @Override
     public Object partTwo(Stream<String> lines) {
@@ -72,7 +70,7 @@ public class Day08 extends Day {
         List<String> directions = Arrays.stream(allLines.get(0).split("")).toList();
 
         Map<String, Node> network = new HashMap<>();
-        List<Node> currentNodes = new ArrayList<>();
+        List<Node> startNodes = new ArrayList<>();
 
         for (String line : allLines.stream().skip(2).toList()) {
 
@@ -90,47 +88,53 @@ public class Day08 extends Day {
                 root.rightPointer = right;
                 network.put(root.value, root);
                 if (root.value.endsWith("A")) {
-                    currentNodes.add(root);
+                    startNodes.add(root);
                 }
             }
         }
-        System.out.println(currentNodes);
 
         // traverse for each branch based on instructions
+        long totalSteps = 1L;
         long steps = 0L;
 
-        int i = 0;
-        while (!allTargetsReached(currentNodes)) {
-            List<Node> nextNodes = new ArrayList<>();
-            for (Node currentNode : currentNodes) {
+        for (Node currentNode : startNodes) {
+            int i = 0;
+            while (!targetReached(currentNode)) {
                 if (directions.get(i).equals("L")) {
                     currentNode = network.get(currentNode.value).leftPointer;
                 }
                 if (directions.get(i).equals("R")) {
                     currentNode = network.get(currentNode.value).rightPointer;
                 }
-                nextNodes.add(currentNode);
+                i = (i + 1) % directions.size();
+                steps++;
             }
-            currentNodes = nextNodes;
-            i = (i + 1) % directions.size();
-            steps++;
 
-            List<Node> filteredNodes = currentNodes.stream().filter(node -> node.value.endsWith("Z")).toList();
-            if (filteredNodes.size() != 0) {
-                System.out.println(filteredNodes);
-                return steps;
-            }
+            totalSteps = calculateLeastCommonMultiple(
+                    totalSteps,
+                    steps,
+                    calculateHighestCommonFactor(totalSteps, steps)
+            );
+            steps = 0;
         }
-
-        return steps;
+        return totalSteps;
     }
 
-    boolean allTargetsReached(List<Node> currentNodes) {
-        for (Node node : currentNodes) {
-            if (!node.value.endsWith("Z")) {
-                return false;
+    boolean targetReached(Node node) {
+        return node.value.endsWith("Z");
+    }
+
+    long calculateHighestCommonFactor(long x, long y) {
+        long min = Math.min(x, y);
+        for (long i = min; i > 0; i--) {
+            if (x % i == 0 && y % i == 0) {
+                return i;
             }
         }
-        return true;
+        return min;
+    }
+
+    long calculateLeastCommonMultiple(long x, long y, long hcf) {
+        return x * y / hcf;
     }
 }
