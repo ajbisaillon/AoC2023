36cd8ea779a18535dd54ed710ae2ab1a3b00b95d
Adrienne Bisaillon <adrienne.bisaillon@gmail.com>
2023-12-29 21:33:47 -0500

    Day 15 part 2

--- src/aoc2023/day15/Day15.java
+++ src/aoc2023/day15/Day15.java
@@ -2,7 +2,7 @@ package aoc2023.day15;
 
 import aoc2023.Day;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Day15 extends Day {
@@ -26,6 +26,55 @@ public class Day15 extends Day {
 
     @Override
     public Object partTwo(Stream<String> lines) {
-        return null;
+        final List<List<String>> boxes = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+            boxes.add(new ArrayList<>());
+        }
+
+        Arrays.stream(lines.findFirst().get().split(",")).forEach(step -> {
+            String label = "";
+            String focalLength = null;
+            if (step.contains("=")) {
+                String[] parts = step.split("=");
+                label = parts[0];
+                focalLength = parts[1];
+            } else if (step.contains("-")) {
+                String[] parts = step.split("-");
+                label = parts[0];
+            }
+
+            int boxNumber = 0;
+            for (int ch : label.toCharArray()) {
+                boxNumber += ch;
+                boxNumber *= 17;
+                boxNumber %= 256;
+            }
+
+            List<String> box = boxes.get(boxNumber);
+            String finalLabel = label;
+            Optional<String> found = box.stream().filter(lensLabel -> lensLabel.startsWith(finalLabel)).findFirst();
+            if (focalLength != null) {
+                if (found.isPresent()) {
+                    int index = box.indexOf(found.get());
+                    box.set(index, String.format("%s %s", label, focalLength));
+                } else {
+                    box.add(String.format("%s %s", label, focalLength));
+                }
+            } else {
+                found.ifPresent(box::remove);
+            }
+        });
+
+
+        int result = 0;
+        for (int i = 0; i < boxes.size(); i++) {
+            List<String> box = boxes.get(i);
+            for (int j = 0; j < box.size(); j++) {
+                String lens = box.get(j);
+                int fl = Integer.parseInt(lens.substring(lens.length() - 1));
+                result += (i + 1) * (j + 1) * fl;
+            }
+        }
+        return result;
     }
 }
